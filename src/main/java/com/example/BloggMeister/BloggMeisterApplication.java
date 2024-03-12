26d4f92b8307c6e8942b3aa66b1c7a18ee47386b
KilRoy95 <Leopwn@hotmail.com>
2024-03-12 14:28:48 +0100

    Initial Commit

---
+++ src/main/java/com/example/BloggMeister/BloggMeisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.BloggMeister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggMeisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggMeisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BloggMeister/BloggMeisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BloggMeister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggMeisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
